5d256e556e590b60c15b7bbb7811848b00d598c4
iida <iida@tangit.jp>
2011-03-15 21:26:47 +0900

    modify path separator

--- app/config/site.php
+++ app/config/site.php
@@ -48,8 +48,8 @@ $config['Mail'] = array(
 );
 
 $config['ReceiveMail'] = array(
-	'stopfile_path' => APP . "tmp" . DS . "stop.file",
-	'ps_log_path' => APP . "app" . DS . "tmp" . DS . "ps.log",
+	'stopfile_path' => APP . "tmp/stop.file",
+	'ps_log_path' => APP . "app/tmp/ps.log",
 	'mail_dir_new' => '/home/'.$config['Defaults']['receive_mail_user'].'/Maildir/new/',
 	'mail_dir_done_normal' => '/home/'.$config['Defaults']['receive_mail_user'].'/Maildir/done/normal/',
 	'mail_dir_done_error' => '/home/'.$config['Defaults']['receive_mail_user'].'/Maildir/done/error/',
@@ -60,11 +60,8 @@ $config['Diary'] = array(
 	'image_thumb_size' => 200,
 	'image_rect_size' => 100,
 	'image_filesize_max' => 2000000,
-//	'image_path_original' => APP . "webroot" . DS . "img" . DS . "photo" . DS . "%s" . DS . "%s_original.jpg",
-//	'image_path_thumb' => APP . "webroot" . DS . "img" . DS . "photo" . DS . "%s" . DS . "%s_thumb.jpg",
-//	'image_path_rect' => APP . "webroot" . DS . "img" . DS . "photo" . DS . "%s" . DS . "%s_rect.jpg",
-	'image_path_original' => "photo" . DS . "%s" . DS . "%s_original.jpg",
-	'image_path_thumb' => "photo" . DS . "%s" . DS . "%s_thumb.jpg",
-	'image_path_rect' => "photo" . DS . "%s" . DS . "%s_rect.jpg",
+	'image_path_original' => "photo/%s/%s_original.jpg",
+	'image_path_thumb' => "photo/%s/%s_thumb.jpg",
+	'image_path_rect' => "photo/%s/%s_rect.jpg",
 );
 
