07aabf3db682277712cd9e591fb2e481d9ce0bb8
nagai <hidetoshing@gmail.com>
2011-03-15 13:52:28 +0900

    update site.php

---
+++ app/config/site.php
@@ -0,0 +1,28 @@
+<?php 
+
+$config['Lines'] = array (
+	'categories' => array(
+		'baby',
+		'petit_f',
+		'petit',
+		'pocket',
+		'hop',
+		'step',
+		'jump',
+	),
+);
+
+$config['Present'] = array(
+	'type' => array(
+		'背景画像' => 0,
+		'教材コンテンツ' => 1, 
+		'待受FLASH' => 2, 
+		'ポストカード' => 3,
+	),
+	'membersonly' => array(
+		'会員限定' => -1,
+	)
+);
+
+
+
