3ecce09d7653cfeed641c8e80187ccf919b9e312
tangit <iida@tangit.jp>
2011-03-16 17:52:18 +0900

    Validation alphaNumeric add , Cent OS Measures .

--- app/app_model.php
+++ app/app_model.php
@@ -9,5 +9,22 @@ class AppModel extends LazyModel {
 		'AutoTransaction',
 	);
 
+        //cent OS 対策
+        function alphaNumeric($check) {
+            $_this =& Validation::getInstance();
+            $_this->__reset();
+            $_this->check = $check;
+
+            if (is_array($check)) {
+                    $_this->_extract($check);
+            }
+
+            if (empty($_this->check) && $_this->check != '0') {
+                    return false;
+            }
+            $_this->regex = '/^[a-z\d]*$/i';
+            return $_this->_check();
+        }
+
 }
 
