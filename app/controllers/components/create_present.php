3b74ffc5022d7db1770944923801e2c733f55c5c
tangit <iida@tangit.jp>
2011-03-16 15:59:19 +0900

    create present comp, onetime url add logic fix.

--- app/controllers/components/create_present.php
+++ app/controllers/components/create_present.php
@@ -2,8 +2,18 @@
 
 class CreatePresentComponent extends Object {
 
-    function createPostCard($args){
+    function createFlash($args){
+        //引数確認
+        if(empty($args)){
+            return false;
+        }
 
+        
+    }
+
+
+    function createPostCard($args){
+        //引数確認
         if(empty($args)){
             return false;
         }
@@ -29,12 +39,27 @@ class CreatePresentComponent extends Object {
         //下地画像へ、テンプレート画像を合成
         ImageCopy($new_image, $template, 0, 0,  0, 0, 400, 592);
 
+        //画像名生成
+        $new_file_name = substr(md5($args['child_id'].time()),0,20);
+
 	//画像保存
-	ImageJPEG($new_image, (WWW_ROOT.sprintf(Configure::read('Present.path.postcard_output'), $args['present_id'])), 100);
+	ImageJPEG($new_image, (WWW_ROOT.sprintf(Configure::read('Present.path.postcard_output'), $new_file_name)), 100);
 
         //メモリを開放します
         imagedestroy($new_image);
 
+        //ワンタイムURL登録
+        $postcard_url =& ClassRegistry::init('postcardUrl');
+        $options = array(
+            'child_id' => 2,
+            'token' => $new_file_name
+            );
+        $postcard_url->create();
+        if (!$postcard_url->save($options)) {
+            //データ登録に失敗した場合、ファイルを消す。
+            unlink( WWW_ROOT.sprintf(Configure::read('Present.path.postcard_output'), $new_file_name) );
+            return false;
+        }
         return true;
 
     }
