8ccffcd0c87fedfa79e223b0904882bf68847073
tangit <iida@tangit.jp>
2011-03-22 16:53:21 +0900

    decome test.

--- app/controllers/diaries_controller.php
+++ app/controllers/diaries_controller.php
@@ -357,36 +357,18 @@ Content-Type: text/html; charset=Shift_JIS
 Content-Transfer-Encoding: 8bit
 ';
 
-if ($diary['Diary']['has_image']) {
 $list[1] = '
 <html>
 <title>'.$diary['Diary']['title'].'</title>
 <body bgcolor="#FFFF8E">
 
 <div align="center"><img src="cid:00" width="50" hight="50"></div>
-<div align="center"><img src="cid:01"></div>
 <div align="center">'.$diary['Diary']['body'].'</div>
-<div align="center"><img src="cid:02" width="50" hight="50"></div>
 
 </body>
 </html>
 
 ';
-} else {
-   $list[1] = '
-<html>
-<title>'.$diary['Diary']['title'].'</title>
-<body bgcolor="#FFFF8E">
-
-<div align="center"><img src="cid:00" width="50" hight="50"></div>
-<div align="center">'.$diary['Diary']['body'].'</div>
-<div align="center"><img src="cid:02" width="50" hight="50"></div>
-
-</body>
-</html>
-
-';
-}
 
 if($diary['Month']['month'] < 10) {
     $imgMonth = '0'.$diary['Month']['month'];
@@ -405,65 +387,21 @@ $jpeg_enc = base64_encode($img);
 $list[3] = $jpeg_enc;
 
 
-if ($diary['Diary']['has_image']) {
-$list[4] = '
---5000000000
-Content-Type: image/jpeg; name='.$diary['Diary']['id'].'.jpg'.'
-Content-Transfer-Encoding: base64
-Content-ID: <01>
-
-';
-$img = file_get_contents('img/'.sprintf(Configure::read('Diary.image_path_thumb'), $diary['Diary']['child_id'], $diary['Diary']['id']));
-$jpeg_enc = base64_encode($img);
-$list[5] = $jpeg_enc;
-
-$list[6] = '
---5000000000
-Content-Type: image/jpeg; name='.'diaryback_'.$diary['Month']['year'].$imgMonth.'_footer.jpg'.'
-Content-Transfer-Encoding: base64
-Content-ID: <02>
-
-';
-
-$img = file_get_contents(sprintf(Configure::read('Present.path.diaryback_f'), $diary['Month']['year'], $imgMonth));
-$jpeg_enc = base64_encode($img);
-$list[7] = $jpeg_enc;
-
-
-$list[8] ='
---5000000000--
-';
-} else {
-$list[4] = '
---5000000000
-Content-Type: image/jpeg; name='.'diaryback_'.$diary['Month']['year'].$imgMonth.'_footer.jpg'.'
-Content-Transfer-Encoding: base64
-Content-ID: <02>
-
-';
-
-$img = file_get_contents(sprintf(Configure::read('Present.path.diaryback_f'), $diary['Month']['year'], $imgMonth));
-$jpeg_enc = base64_encode($img);
-$list[5] = $jpeg_enc;
-
-$list[6] ='
---5000000000--
-';
-}
         while(list($key,$value) = each($list)){
                 $value = mb_convert_encoding($value,  'Shift_JIS', 'UTF-8');
                 @fwrite( $fp, $value, strlen($value) );
         }
 
 
+        
         fclose($fp);
         //ファイルへの書き込みは終了
 
         $file_length = filesize($filepath);
         header("Content-Disposition: attachment; filename=$filepath");
         header("Content-Length:$file_length");
-        header("Content-Type: application/x-smaf");
-        //print $data;
+        header("Content-Type: application/octet-stream");
+
         readfile ($filepath);
 
     }
