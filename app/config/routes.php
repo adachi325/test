1c6bc079d2cb980475d8886eb81103975c09f2be
nagai <hidetoshing@gmail.com>
2011-03-29 17:18:36 +0900

    change routing rule

--- app/config/routes.php
+++ app/config/routes.php
@@ -29,7 +29,8 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/views/pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'top'));
+	//Router::connect('/', array('controller' => 'pages', 'action' => 'top'));
+	Router::connect('/', array('controller' => 'children', 'action' => 'index'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
