3c41637359e75b9ee1754a6ac83024f9f43564cb
tantan <iida@tangit.jp>
2011-04-05 12:27:12 +0900

    fixed.

--- app/tests/cases/models/present.test.php
+++ app/tests/cases/models/present.test.php
@@ -44,12 +44,14 @@ class PresentTestCase extends CakeTestCase {
 		$options['4'] = array('type' => 1, 'child_id' => 'e329hd2',);
 		$options['5'] = array('type' => 4, 'child_id' => 'a',);
 		$options['6'] = array('type' => 3, 'child_id' => '',);
+                $options['6'] = array('type' => '</div>', 'child_id' => '1',);
+                $options['6'] = array('type' => 3, 'child_id' => '<div>',);
 
                 $i=1;
                 foreach($options as $option) {
                     $result = $p->find($type,$option);
                     if(isset($result['0'])){
-                        $this->assertNull($result['0'], '異常系'.$i);
+                        $this->assertNull($result['0'], '[type]異常系'.$i);
                     }else {
                         $this->assertNull(null, '[type]異常系'.$i);
                     }
@@ -69,6 +71,31 @@ class PresentTestCase extends CakeTestCase {
                 foreach($options as $option) {
                     $result = $p->find($type,$option);
                     $this->assertNotNull($result, '[month]正常系'.$i);
+                    pr($result);
+                    pr($type.','.$option['year'].','.$option['month']);
+                    $i++;
+                }
+
+                //異常系MONTH
+		$type = "month";
+                $options = array();
+		$options['0'] = array('year' => '', 'month' => 3,);
+                $options['1'] = array('year' => 'あああ', 'month' => 4,);
+                $options['2'] = array('year' => 'aaa?', 'month' => 5,);
+                $options['3'] = array('year' => '</html>', 'month' => 5,);
+  		$options['4'] = array('year' => '2011', 'month' => 'aaa',);
+                $options['5'] = array('year' => '2011', 'month' => '',);
+                $options['6'] = array('year' => '2011', 'month' => 3724728734,);
+                $options['7'] = array('year' => '<[in]>', 'month' => 3724728734,);
+
+                $i=1;
+                foreach($options as $option) {
+                    $result = $p->find($type,$option);
+                    if(isset($result['0'])){
+                        $this->assertNull($result['0'], '[month]異常系'.$i);
+                    }else {
+                        $this->assertNull(null, '[month]異常系'.$i);
+                    }
                     pr($result);
                     pr($type.','.$option['year'].','.$option['month']);
                     $i++;
