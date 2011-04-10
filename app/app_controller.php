62349c1205b9275b3cb665af5754199654e952ff
tantan <iida@tangit.jp>
2011-04-10 16:46:34 +0900

    fixed.

--- app/app_controller.php
+++ app/app_controller.php
@@ -111,12 +111,11 @@ class AppController extends Controller {
                 $this->log($this->data,LOG_DEBUG);
                 if(!isset($this->params['form']['csid'])){
                     $this->log('-----2-----',LOG_DEBUG);
-                    $this->log('-----3-----',LOG_DEBUG);
                     $keys = array_keys($this->data);
                     $this->log($keys,LOG_DEBUG);
                     if(isset($this->data[$keys[0]]['session_id'])){
                         $this->params['form'] = array(session_name() => $this->data[$keys[0]]['session_id']);
-                        $this->log('-----4-----',LOG_DEBUG);
+                        $this->log('-----3-----',LOG_DEBUG);
                         $this->log($this->data[session_name()]['session_id'],LOG_DEBUG);
                     }
                 }
