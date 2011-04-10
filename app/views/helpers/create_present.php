a9ddd69ab2fa7c38ca0a48976a83c3450d80dd1b
tantan <iida@tangit.jp>
2011-04-11 01:13:54 +0900

    fix.

--- app/views/helpers/create_present.php
+++ app/views/helpers/create_present.php
@@ -1,9 +1,12 @@
 <?php
 require_once APP.'controllers'.DS.'components'.DS.'create_present.php';
+
+
 class CreatePresentHelper extends Helper {
 
     function __call($methodName, $args)
     {
+        pr(APP.'controllers'.DS.'components'.DS.'create_present.php');
         $create_present = new CreatePresentComponent();
         return call_user_func_array(array($create_present, $methodName), $args);
     }
