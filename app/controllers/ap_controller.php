b0002bcd0e5de57768bc2bd18ae9bacd69d29ad7
tantan <tadokoro@tangit.jp>
2011-04-15 23:42:58 +0900

    fix.

--- app/controllers/ap_controller.php
+++ app/controllers/ap_controller.php
@@ -161,6 +161,9 @@ class ApController extends AppController {
 
 		$release_date = $data['Content']['release_date'];
 
+		pr($release_date);
+		pr(date('Y-m-d h:n:s'));
+
 		if ($release_date <= date('Y-m-d h:n:s')) {
 			$filepath = WWW_ROOT."ap/{$line}/{$id}/index.html";
 
