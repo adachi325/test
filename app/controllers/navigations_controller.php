837c74c0ed2bf21409db5b0c50e1e10eec38f069
tantan <tadokoro@tangit.jp>
2011-04-18 20:17:10 +0900

    mail bug fix.

--- app/controllers/navigations_controller.php
+++ app/controllers/navigations_controller.php
@@ -59,7 +59,7 @@ class NavigationsController extends AppController {
 		$mailStr = 'diary_'.$userdata['User']['id'].'.'.$userdata['User']['last_selected_child'].'.'.$themes[0]['Theme']['id'].'.'.$hash.'@'.Configure::read('Defaults.domain');
 
 		//メールタイトル設定
-		$mailTitle = 'ベスト ショット';
+		$mailTitle = 'ベストショット';
 
 		$ua = $_SERVER['HTTP_USER_AGENT'];
 
