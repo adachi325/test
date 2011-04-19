38a546afee7850f0dcd2022c2eb4ea74e1f1885d
hidetoshing <hidetoshing@gmail.com>
2011-04-19 12:08:59 +0900

    select presents

--- app/controllers/presents_controller.php
+++ app/controllers/presents_controller.php
@@ -141,7 +141,6 @@ class PresentsController extends AppController {
 					$this->Session->write('Present.data.type', $type);
 					$this->redirect("/presents/complete/");
 				} else {
-					$this->Session->setFlash('選択数が不正です');
 					$this->Session->write('Present.error.type', $type);
 					$this->Session->write('Present.error.template_id', $template_id);
 					$this->redirect("/presents/error_photo/");
@@ -174,7 +173,7 @@ class PresentsController extends AppController {
 		$items = $this->paginate('Diary', $cond);
 
 		//思い出の投稿すうがプレゼント作成に必要な枚数以下の場合エラー
-		if(count($items) < $max_count){
+		if ((count($items) < $max_count) && ($page == 1)) {
 			$this->redirect("/presents/error_present/");
 		}
 
