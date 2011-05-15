e44b9a5bc1423cb3d1d6671abaed2face00e4757
tantan <tadokoro@tangit.jp>
2011-05-16 03:24:57 +0900

    ssl user register loigic fixed.

--- app/app_controller.php
+++ app/app_controller.php
@@ -127,12 +127,8 @@ class AppController extends Controller {
 		if($this->Ktai->is_imode()) {
 		    //SSLページでのUIDチェック用
 		    $ssluid= $this->Session->read('sslUid');
-			$this->log('みそ１',LOG_DEBUG);
-			$this->log($ssluid,LOG_DEBUG);
 		    if(empty($ssluid) || !isset($ssluid)){
 			$uid = $this->Ktai->get_uid();
-			$this->log('みそ２',LOG_DEBUG);
-			$this->log($uid,LOG_DEBUG);
 			if(isset($uid)){
 			    $this->Session->write('sslUid', $uid);
 			    $this->log($this->Session->read('sslUid'),LOG_DEBUG);
