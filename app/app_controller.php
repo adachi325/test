2570d9363b68338dd72a564d8fa23b96d3e08b6d
tantan <tadokoro@tangit.jp>
2011-05-16 02:32:01 +0900

    ssl test

--- app/app_controller.php
+++ app/app_controller.php
@@ -129,6 +129,9 @@ class AppController extends Controller {
 		    $ssluid= $this->Session->read('sslUid');
 		    if(!isset($ssluid)){
 			$uid = $this->Ktai->get_uid();
+
+				$this->log('みそ',LOG_DEBUG);
+				$this->log($uid,LOG_DEBUG);
 			if(isset($uid)){
 			    $this->Session->write('sslUid', $uid);
 			    $this->log($this->Session->read('sslUid'),LOG_DEBUG);
@@ -161,9 +164,6 @@ class AppController extends Controller {
 			    $ssluid= $this->Session->read('sslUid');
 			    if(!isset($ssluid)){
 				$uid = $this->Ktai->get_uid();
-				$this->log('みそ',LOG_DEBUG);
-				$this->log($uid,LOG_DEBUG);
-
 				if(isset($uid)){
 				    $this->Session->write('sslUid', $uid);
 				    $this->log($this->Session->read('sslUid'),LOG_DEBUG);
