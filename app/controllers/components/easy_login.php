4590916604330b6f61600a7fd67d4dc5cd1c639b
tantan <tadokoro@tangit.jp>
2011-05-16 12:12:32 +0900

    ssl test

--- app/controllers/components/easy_login.php
+++ app/controllers/components/easy_login.php
@@ -78,7 +78,11 @@ class EasyLoginComponent extends Object {
             $User = ClassRegistry::init('User');
 
             //個体識別番号取得
-            $this->mobuid = $this->_getUid();
+	    if (isset($_SERVER['HTTPS'])) {
+		$this->mobuid = $this->Session->read('sslUid');
+	    } else {
+		$this->mobuid = $this->_getUid();
+	    }
 
             //簡単ログイン個体番号が設定されている場合
             if($this->mobuid!='') {
@@ -102,7 +106,13 @@ class EasyLoginComponent extends Object {
                 $User = ClassRegistry::init('User');
                 $request = array();
                 $request['User']['id'] = $selectId;
-                $request['User']['uid'] = $this->_getUid();
+
+		//個体識別番号取得
+		if (isset($_SERVER['HTTPS'])) {
+		    $request['User']['uid'] = $this->Session->read('sslUid');
+		} else {
+		    $request['User']['uid'] = $this->_getUid();
+		}
 
                 //$selectidがnullならログアウト
                 if(empty($selectId) or !isset($selectId)){
