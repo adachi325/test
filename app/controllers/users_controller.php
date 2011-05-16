adb19d8760e3476728a3d8fffbf748c0e5f2b02b
tantan <tadokoro@tangit.jp>
2011-05-16 15:11:41 +0900

    ssl test

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -350,7 +350,7 @@ class UsersController extends AppController {
     function remind () {
 
 	//初回はNoCheck
-	if (!eregi("\/remind\/", $this->referer())) {
+	if (!eregi("remind", $this->referer())) {
 	    return;
 	}
 
