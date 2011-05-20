e724f9effa1dc990dd43f3c1753398fef9ea9e0b
hidetoshing <hidetoshing@gmail.com>
2011-05-20 16:40:39 +0900

    add upgrade shell

---
+++ vendors/shells/upgrade.php
@@ -0,0 +1,46 @@
+<?php
+
+App::import('Shell', 'AppShell');
+class UpgradeShell extends AppShell {
+	
+	function main() {
+		$this->addhash();
+		$this->sethash();
+	}
+
+	function addhash() {
+		$User =& ClassRegistry::init('User');
+		$Child =& ClassRegistry::init('Child');
+
+		echo '-----'.PHP_EOL;
+		$data = $User->query('ALTER TABLE users ADD hash VARCHAR(10) DEFAULT NULL;');
+		if ($data) {
+			echo 'add hash to users'.PHP_EOL;
+		}
+		$data = $Child->query('ALTER TABLE children ADD hash VARCHAR(10) DEFAULT NULL;');
+		if ($data) {
+			echo 'add hash to children'.PHP_EOL;
+		}
+		echo '-----'.PHP_EOL;
+
+	}
+
+	function sethash() {
+		$this->User =& ClassRegistry::init('User');
+		$this->Child =& ClassRegistry::init('Child');
+		
+		$this->User->contain();
+		$users = $this->User->find('all');
+		foreach ($users as $user) {
+			$this->User->save_hashcode($user['User']['id']);
+		}
+
+		$this->Child->contain();
+		$children = $this->Child->find('all');
+		foreach ($chldren as $child) {
+			$this->Child->save_hashcode($child['Child']['id']);
+		}
+	}
+
+
+}
