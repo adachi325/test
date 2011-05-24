05f4f4c5a45bab62b37ebe4a40600d9c4761b72c
tangit-kimura <kimura@tangit.jp>
2011-05-24 18:24:15 +0900

    add getHanamaruCount testcases.

--- app/tests/cases/models/hanamaru.test.php
+++ app/tests/cases/models/hanamaru.test.php
@@ -19,6 +19,18 @@ class HanamaruTestCase extends CakeTestCase {
     // 存在しないユーザーで実施
     $count = $this->Hanamaru->getHanamaruCount(242424);
     $this->assertEqual($count, 0);
+
+    // 引数無しで実施
+    $count = $this->Hanamaru->getHanamaruCount();
+    $this->assertEqual($count, 0);
+    
+    // nullで実施
+    $count = $this->Hanamaru->getHanamaruCount(null);
+    $this->assertEqual($count, 0);
+    
+    // 文字列で実施
+    $count = $this->Hanamaru->getHanamaruCount("hogehoge");
+    $this->assertEqual($count, 0);
   }
 
 	function endTest() {
