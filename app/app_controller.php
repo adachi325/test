f174a611538dfd64f0b80b39087b3244310f53ed
Tadokoro <tadokoro@tangit.jp>
2011-06-21 19:04:54 +0900

    log logic remove

--- app/app_controller.php
+++ app/app_controller.php
@@ -150,14 +150,12 @@ class AppController extends Controller {
 			session_id($_REQUEST[$session_name]);
 			output_add_rewrite_var($session_name, $_REQUEST[$session_name]);
 		    }
-
 		    // auでフォームからの入力が文字化けする問題の対策
 		    if ($this->Ktai->is_ezweb()) {
 			if (!empty($this->data)) {
 			    mb_convert_variables('UTF-8', 'UTF-8, sjis-win', $this->data);
 			}
 		    }
-
 		}
 
 		//SSL通信環境設定
