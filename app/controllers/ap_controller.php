87b72a1c874edbc61fcce447c5e1b65d6b20ec73
hidetoshing <hidetoshing@gmail.com>
2011-07-11 14:24:12 +0900

    fixed content order

--- app/controllers/ap_controller.php
+++ app/controllers/ap_controller.php
@@ -228,40 +228,40 @@ class ApController extends AppController {
 
 		$release_date = $data['Content']['release_date'];
 
-		if ($release_date <= date('Y-m-d H:i:s')) {
-			$filepath = WWW_ROOT."ap/{$line}/{$id}/index.html";
+        if ($release_date <= date('Y-m-d H:i:s')) {
+            $filepath = WWW_ROOT."ap/{$line}/{$id}/index.html";
 
-			$this->layout = 'contents';
-			if ($this->Ktai->is_softbank()) {
-				$_path = WWW_ROOT."ap/{$line}/{$id}/index.softbank.html";
-			} elseif ($this->Ktai->is_ezweb()) {
-				$_path = WWW_ROOT."ap/{$line}/{$id}/index.au.html";
-			} elseif ($this->Ktai->is_android()) {
-				$_path = WWW_ROOT."ap/{$line}/{$id}/index.android.html";
-				if (!file_exists($_path)) {
-					$this->layout = null;
-					$this->render('android');
-					return;
+            $this->layout = 'contents';
+            if ($this->Ktai->is_softbank()) {
+                $_path = WWW_ROOT."ap/{$line}/{$id}/index.softbank.html";
+            } elseif ($this->Ktai->is_ezweb()) {
+                $_path = WWW_ROOT."ap/{$line}/{$id}/index.au.html";
+            } elseif ($this->Ktai->is_android()) {
+                $_path = WWW_ROOT."ap/{$line}/{$id}/index.android.html";
+                if (!file_exists($_path)) {
+                    $this->layout = null;
+                    $this->render('android');
+                    return;
+                } else {
+                    $this->layout = 'contents_android';
+                }
+            }
+
+            if (isset($_path) && file_exists($_path)) {
+                $filepath = $_path;
+            }
+
+            $this->set(compact('release_date', 'filepath'));
+            $this->render("view");
         } else {
-			    $this->layout = 'contents_android';
+            $this->set(compact('release_date'));
+            if ($this->Ktai->is_android()) {
+                $this->layout = null;
+                $this->render("android_error");	
+            } else {
+                $this->render("error");	
+            }
         }
-			}
-
-			if (isset($_path) && file_exists($_path)) {
-				$filepath = $_path;
-			}
-			
-			$this->set(compact('release_date', 'filepath'));
-			$this->render("view");
-		} else {
-			$this->set(compact('release_date'));
-			if ($this->Ktai->is_android()) {
-				$this->layout = null;
-				$this->render("android_error");	
-			} else {
-				$this->render("error");	
-			}
-		}
 	}
 
 }
